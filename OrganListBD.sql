80b6d12d1fe31e89f29833008794998d15d54b6d
MarioFS <mariofernandezs1@gmail.com>
2021-10-21 21:11:14 +0200

    BBDD completed

--- OrganListBD.sql
+++ OrganListBD.sql
@@ -14,11 +14,20 @@ create table Lista(
 	id int unsigned primary key auto_increment,
 	nombre varchar(70) not null,
 	favorita boolean not null,
-	descripcion varchar(255) null
+	descripcion varchar(255) null,
+	usuario int unsigned not null
 );
 
 alter table Lista
-	add constraint FK_usuario_lista foreign key (id);
+	add constraint FK_usuario_id foreign key (usuario) references Usuario(id);
+
+create table Producto(
+	id int unsigned primary key auto_increment,
+	nombre varchar(70) not null,
+	descripcion varchar(255) null,
+	esencial boolean not null,
+	categoria varchar(100) not null
+);
 
 create table ListaProducto(
 	id int unsigned primary key auto_increment,
@@ -27,18 +36,9 @@ create table ListaProducto(
 );
 
 alter table ListaProducto
-	add constraint FK_relacion_lista foreign key (id);
-
-create table Producto(
-	id int unsigned primary key auto_increment,
-	nombre varchar(70) not null,
-	descripcion varchar(255) null,
-	esencial boolean not null
-	categoria varchar(100) not null
-);
-
-alter table Producto
-	add constraint FK_relacion_producto foreign key (id);
+	add constraint FK_lista_id foreign key (lista) references Lista(id);
+alter table ListaProducto
+    add constraint FK_producto_id foreign key (producto) references Producto(id);
 
 create table ProductoTienda(
 	id int unsigned primary key auto_increment,
@@ -47,14 +47,13 @@ create table ProductoTienda(
 	precio decimal(6, 2) null
 );
 
-alter table ProductoTienda
-	add constraint FK_relacion_producto_tienda foreign key (id);
-
 create table Tienda(
 	id int unsigned primary key auto_increment,
 	nombre varchar(70) not null,
 	tipo varchar(70) not null
 );
 
-alter table Tienda
-	add constraint FK_relacion_tienda foreign key (id);
+alter table ProductoTienda
+	add constraint FK_tienda_id foreign key (tienda) references Tienda(id);
+alter table ProductoTienda
+	add constraint FK_producto_id foreign key (producto) references Producto(id);
