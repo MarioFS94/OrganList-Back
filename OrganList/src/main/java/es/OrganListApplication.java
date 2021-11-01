c3b31c79f9527dbe80e69a4e21426848e419a4ab
MarioFS <mariofernandezs1@gmail.com>
2021-11-01 14:39:31 +0100

    add create archetype documentation and upload maven project

---
+++ OrganList/src/main/java/es/OrganListApplication.java
@@ -0,0 +1,15 @@
+package es;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class OrganListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganListApplication.class, args);
+    }
+
+}
---
+++ OrganList/src/main/java/es/controller/OrganListController.java
@@ -0,0 +1,37 @@
+package es.controller;
+
+import es.utils.DefaultDocumentation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@Tag(name = "OrganListController", description = "Initial controller")
+public class OrganListController {
+
+    @Operation(summary = "Welcome endpoint", description = "Welcome!!", responses = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "You are welcome!",
+                    content = @Content(schema = @Schema(implementation = String.class))
+            )
+    })
+    //Common ApiResponses
+    @DefaultDocumentation
+    @GetMapping
+    public String index(
+            @RequestParam(required = false)
+            @Parameter(description = "Nombre del autor") String name
+    ) {
+        return name == null ? "Bienvenido a OrganList!" : "Bienvenido " + name + " a OrganList!";
+    }
+
+}
